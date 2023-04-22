12bee4460de26c373c93ca4f7ff52911ab0adf3f
Ivan, David <avandavad@gmail.com>
2023-04-22 09:34:11 +0200

    annotation - load image

--- src/annotator.py
+++ src/annotator.py
@@ -2,15 +2,38 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QPushButton, QFileDialog, QWidget
 from PyQt5.QtGui import QPixmap
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Simple PyQt5 Window")
-        self.setGeometry(100, 100, 400, 300)
+        self.setWindowTitle("Image Display in PyQt5")
+        self.setGeometry(100, 100, 800, 600)
 
-        label = QLabel("Hello, PyQt5!", self)
-        label.adjustSize()
+        self.initUI()
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
+
+        self.image_label = QLabel(self)
+        layout.addWidget(self.image_label)
+
+        button = QPushButton("Load Image", self)
+        layout.addWidget(button)
+        button.clicked.connect(self.load_image)
+
+    def load_image(self):
+        file, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Images (*.png *.xpm *.jpg)")
+
+        if file:
+            pixmap = QPixmap(file)
+            self.image_label.setPixmap(pixmap)
+            self.image_label.setScaledContents(True)
+            self.image_label.adjustSize()
 
 
 if __name__ == "__main__":
