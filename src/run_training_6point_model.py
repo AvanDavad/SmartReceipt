e43ebec5164c5bced4e718ad9b2e9ce2f0f81ed1
Ivan, David <avandavad@gmail.com>
2023-06-10 17:26:30 +0200

    refac: training CNNModule6Points

--- src/run_training_6point_model.py
+++ src/run_training_6point_model.py
@@ -1,34 +1,48 @@
+import argparse
 from pytorch_lightning import Trainer
+from src.image_reader import ImageReader
 from src.models import CNNModule6Points
-from src.reader import ImageDataset, ImageReader
+from src.image_dataset import ImageDataset
 from torch.utils.data import DataLoader
 from pathlib import Path
 from pytorch_lightning.callbacks import ModelCheckpoint
 import time
 
-PROJ_DIR = Path("/home/avandavad/projects/receipt_extractor")
-
 
 def main():
-    train_reader = ImageReader(PROJ_DIR / "data" / "train")
-    val_reader = ImageReader(PROJ_DIR / "data" / "val")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train_data", type=str, default="/home/avandavad/projects/receipt_extractor/data/train")
+    parser.add_argument("--val_data", type=str, default="/home/avandavad/projects/receipt_extractor/data/val")
+    parser.add_argument("--from_scratch", action="store_true")
+    parser.add_argument("--version_num", type=int, default=0)
+    parser.add_argument("--ckpt_name", type=str, default="resnet-epoch=65-val_loss=0.00149")
+    parser.add_argument("--model_checkpoints", type=str, default="/home/avandavad/projects/receipt_extractor/model_checkpoints")
+    parser.add_argument("--batch_size", type=int, default=32)
+    args = parser.parse_args()
+
+    train_reader = ImageReader(args.train_data)
+    val_reader = ImageReader(args.val_data)
 
     train_dataset = ImageDataset(train_reader, augment=True)
     val_dataset = ImageDataset(val_reader, augment=False)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=16, num_workers=4)
-    val_dataloader = DataLoader(val_dataset, batch_size=16, num_workers=4)
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=4)
+    val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=4)
 
-    version_num = 18
-    ckpt_name = "resnet-epoch=65-val_loss=0.00149"
-    ckpt_path = (
-        PROJ_DIR
-        / "lightning_logs"
-        / f"version_{version_num}"
-        / "checkpoints"
-        / f"{ckpt_name}.ckpt"
-    )
-    model = CNNModule6Points().load_from_checkpoint(ckpt_path)
+    if args.from_scratch:
+        model = CNNModule6Points()
+    else:
+        version_num = args.version_num
+        ckpt_name = args.ckpt_name
+        ckpt_path = (
+            Path(args.model_checkpoints)
+            / "CNNModule6Points"
+            / "lightning_logs"
+            / f"version_{version_num}"
+            / "checkpoints"
+            / f"{ckpt_name}.ckpt"
+        )
+        model = CNNModule6Points().load_from_checkpoint(ckpt_path)
 
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
@@ -38,6 +52,7 @@ def main():
     )
 
     trainer = Trainer(
+        default_root_dir=Path(args.model_checkpoints) / "CNNModule6Points",
         accelerator="gpu",
         max_epochs=25000,
         callbacks=[checkpoint_callback],
