97773e722c3670cb2321a685f9e0cd7b7b78674b
Ivan, David <avandavad@gmail.com>
2023-06-10 16:59:37 +0200

    refac: line dataset

---
+++ src/line_dataset.py
@@ -0,0 +1,109 @@
+from src.camera_calib import get_camera_calib
+from src.draw_utils import save_img_with_kps
+from src.image_dataset import ImageDataset
+from src.warp_perspective import warp_perspective
+
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+from pathlib import Path
+
+
+class LineDataset(Dataset):
+    def __init__(self, reader, augment=False, shuffle=False):
+        self.reader = reader
+        self.augment = augment
+
+        self.mapping = []
+        for i in range(len(self.reader)):
+            s = self.reader[i]
+            if "lines" in s:
+                num_lines = len(s["lines"]) // 2 - 1
+                for j in range(num_lines):
+                    self.mapping.append((i, j, num_lines))
+
+        if shuffle:
+            self.idx_list = np.random.permutation(len(self.mapping))
+        else:
+            self.idx_list = np.arange(len(self.mapping))
+
+    def __len__(self):
+        return len(self.mapping)
+
+    def __getitem__(self, idx):
+        idx_mod = self.idx_list[idx]
+        i, j, num_lines = self.mapping[idx_mod]
+        s = self.reader[i]
+
+        img = s["img"]
+        line_kps = np.array(s["lines"])
+
+        is_last = j == num_lines - 1
+        is_first = j == 0
+
+        base_kps = np.array(s["keypoints"])
+        if self.augment:
+            base_kps += np.random.randn(*base_kps.shape) * 5.0
+
+        camera_matrix, dist_coeffs = get_camera_calib()
+        img, base_kps, M = warp_perspective(
+            img, base_kps, camera_matrix, dist_coeffs, scale_factor=10.0, verbose=False
+        )
+        line_kps = cv2.perspectiveTransform(line_kps.reshape(-1, 1, 2), M).reshape(
+            -1, 2
+        )
+        curr_line_kps = line_kps[2 * j : 2 * j + 4]
+
+        crop_x0_max = max(1, min([int(base_kps[0, 0]), int(curr_line_kps[:, 0].min())]))
+        crop_x0 = np.random.randint(0, crop_x0_max) if self.augment else 0
+        crop_x1_min = min(
+            img.width - 1, max([int(base_kps[3, 0]), int(curr_line_kps[:, 0].max())])
+        )
+        crop_x1 = (
+            np.random.randint(crop_x1_min, img.width) if self.augment else img.width
+        )
+        if is_first:
+            y_min = int(base_kps[:4, 1].min())
+            y_max = max(int(line_kps[:2, 1].min()), y_min + 1)
+            crop_y0 = np.random.randint(y_min, y_max) if self.augment else y_min
+        else:
+            prev_line_kps = line_kps[2 * j - 2 : 2 * j + 2]
+            y_min = int(prev_line_kps[:, 1].mean())
+            y_max = max(int(curr_line_kps[:, 1].min()), y_min + 1)
+            crop_y0 = np.random.randint(y_min, y_max) if self.augment else y_min
+        crop_y1 = crop_y0 + crop_x1 - crop_x0
+
+        img = img.crop((crop_x0, crop_y0, crop_x1, crop_y1))
+
+        # draw the bottom part of the image black
+        img = np.array(img)
+        ratio = np.random.randint(3, 10) if self.augment else 6
+        img[int(img.shape[0] / ratio) :, :, :] = 128
+        img = Image.fromarray(img)
+        assert img.width == img.height, img.size
+
+        curr_line_kps = curr_line_kps - np.array([crop_x0, crop_y0])
+
+        curr_line_kps = curr_line_kps / np.array([img.width, img.height])
+        curr_line_kps = curr_line_kps.flatten()
+        curr_line_kps = torch.from_numpy(curr_line_kps)
+
+        img_tensor = ImageDataset.TRANSFORMS(img)
+
+        is_last = torch.tensor([is_last]).float()
+        return img_tensor, curr_line_kps, is_last
+
+    def show(self, idx, out_folder, repeat_idx=0):
+        img_tensor, curr_line_kps, is_last = self[idx]
+
+        img = ImageDataset.img_from_tensor(img_tensor)
+
+        curr_line_kps = curr_line_kps.reshape(-1, 2).numpy() * ImageDataset.IMG_SIZE
+
+        filename = out_folder / f"sample_{idx}_{repeat_idx}.jpg"
+        color = "red" if (is_last.item() == 1.0) else "yellow"
+        save_img_with_kps(img, curr_line_kps, filename, circle_color=color, circle_radius=1)
---
+++ src/run_visualize_line_dataset.py
@@ -0,0 +1,32 @@
+import argparse
+from pathlib import Path
+from src.image_reader import ImageReader
+
+from src.line_dataset import LineDataset
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rootdir", type=str, default="/home/avandavad/projects/receipt_extractor/data/train")
+    parser.add_argument("--augment", action="store_true")
+    parser.add_argument("--shuffle", action="store_true")
+    parser.add_argument("--repeat", type=int, default=1)
+    parser.add_argument("--out_folder", type=str, default="visualization/line_dataset")
+
+    args = parser.parse_args()
+
+    dataset = LineDataset(
+        ImageReader(args.rootdir),
+        augment=args.augment,
+        shuffle=args.shuffle,
+    )
+
+    out_folder = Path(args.out_folder)
+    out_folder.mkdir(parents=True, exist_ok=True)
+    for file in out_folder.iterdir():
+        file.unlink()
+
+    for idx in range(len(dataset)):
+        for repeat_idx in range(args.repeat):
+            dataset.show(idx, out_folder, repeat_idx=repeat_idx)
