f14794ac4175cb1eeaa34bc0a72e49b9df827e3f
Ivan, David <avandavad@gmail.com>
2023-08-12 20:12:06 +0200

    tests for phase0 model

---
+++ src/models/test/phase0pointsmodel_test.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import tempfile
+import unittest
+
+from src.models.phase0points_model import CNNModulePhase0Points
+
+PROJ_DIR = Path(__file__).parent.parent.parent.parent
+
+class TestPhase0PointsModel(unittest.TestCase):
+    def setUp(self):
+        self.model = CNNModulePhase0Points()
+
+    def test_inference(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            self.model.inference(
+                PROJ_DIR / "test_data" / "train" / "IMG_0001.jpg",
+                out_folder=Path(tempdir),
+            )
+
+if __name__ == '__main__':
+    unittest.main()
