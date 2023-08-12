498af6c3cfc10aecbf3bd0ff660394404254013f
Ivan, David <avandavad@gmail.com>
2023-08-12 10:51:52 +0200

    src/run/run_training_phase0point_model

--- src/run/run_training_phase0point_model.py
+++ src/run/run_training_phase0point_model.py
@@ -1,8 +1,12 @@
+"""
+python -m src.run.run_training_phase0point_model --from_scratch
+"""
+
 import argparse
 from pytorch_lightning import Trainer
-from src.image_reader import ImageReader
-from src.models import CNNModule6Points
-from src.image_dataset import ImageDataset
+from src.readers.image_reader import ImageReader
+from src.models.phase0points_model import CNNModulePhase0Points
+from src.datasets.phase0points_dataset import Phase0PointsDataset
 from torch.utils.data import DataLoader
 from pathlib import Path
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -10,30 +14,31 @@ import time
 
 
 def main(args):
-
     train_reader = ImageReader(args.train_data)
     val_reader = ImageReader(args.val_data)
 
-    train_dataset = ImageDataset(train_reader, augment=True)
-    val_dataset = ImageDataset(val_reader, augment=False)
+    train_dataset = Phase0PointsDataset(train_reader, augment=True)
+    val_dataset = Phase0PointsDataset(val_reader, augment=False)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=4)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=args.batch_size, num_workers=4
+    )
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=4)
 
     if args.from_scratch:
-        model = CNNModule6Points()
+        model = CNNModulePhase0Points()
     else:
         version_num = args.version_num
         ckpt_name = args.ckpt_name
         ckpt_path = (
             Path(args.model_checkpoints)
-            / "CNNModule6Points"
+            / "CNNModulePhase0Points"
             / "lightning_logs"
             / f"version_{version_num}"
             / "checkpoints"
             / f"{ckpt_name}.ckpt"
         )
-        model = CNNModule6Points().load_from_checkpoint(ckpt_path)
+        model = CNNModulePhase0Points().load_from_checkpoint(ckpt_path)
 
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
@@ -43,7 +48,7 @@ def main(args):
     )
 
     trainer = Trainer(
-        default_root_dir=Path(args.model_checkpoints) / "CNNModule6Points",
+        default_root_dir=Path(args.model_checkpoints) / "CNNModulePhase0Points",
         accelerator="gpu",
         max_epochs=args.max_epochs,
         callbacks=[checkpoint_callback],
@@ -63,12 +68,26 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_data", type=str, default="/home/avandavad/projects/receipt_extractor/data/train")
-    parser.add_argument("--val_data", type=str, default="/home/avandavad/projects/receipt_extractor/data/val")
+    parser.add_argument(
+        "--train_data",
+        type=str,
+        default="/home/avandavad/projects/receipt_extractor/data/train",
+    )
+    parser.add_argument(
+        "--val_data",
+        type=str,
+        default="/home/avandavad/projects/receipt_extractor/data/val",
+    )
     parser.add_argument("--from_scratch", action="store_true")
     parser.add_argument("--version_num", type=int, default=0)
-    parser.add_argument("--ckpt_name", type=str, default="resnet-epoch=65-val_loss=0.00149")
-    parser.add_argument("--model_checkpoints", type=str, default="/home/avandavad/projects/receipt_extractor/model_checkpoints")
+    parser.add_argument(
+        "--ckpt_name", type=str, default="resnet-epoch=65-val_loss=0.00149"
+    )
+    parser.add_argument(
+        "--model_checkpoints",
+        type=str,
+        default="/home/avandavad/projects/receipt_extractor/model_checkpoints",
+    )
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--max_epochs", type=int, default=1000)
 
