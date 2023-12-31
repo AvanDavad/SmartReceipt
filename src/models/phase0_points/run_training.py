e0c3e73841bdeb7cfc58429ae913ccd564062269
Ivan, David <avandavad@gmail.com>
2023-12-31 19:04:55 +0100

    refac: train phase0 model

--- src/models/phase0_points/run_training.py
+++ src/models/phase0_points/run_training.py
@@ -1,6 +1,3 @@
-"""
-python -m src.run.run_training_phase0point_model --from_scratch
-"""
 import argparse
 import time
 from pathlib import Path
@@ -10,9 +7,12 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
 
 from src.datasets.phase0points_dataset import Phase0PointsDataset
-from src.models.phase0points_model import CNNModulePhase0Points
+from src.models.phase0_points.points_model import CNNModulePhase0Points
 from src.readers.image_reader import ImageReader
+from src.run.run_all import get_best_ckpt_path
 
+PROJ_DIR = Path(__file__).parents[2]
+MODEL_CHECKPOINTS = PROJ_DIR / "model_checkpoints" / "CNNModulePhase0Points"
 
 def main(args):
     train_reader = ImageReader(args.train_data)
@@ -31,28 +31,19 @@ def main(args):
     if args.from_scratch:
         model = CNNModulePhase0Points()
     else:
-        version_num = args.version_num
-        ckpt_name = args.ckpt_name
-        ckpt_path = (
-            Path(args.model_checkpoints)
-            / "CNNModulePhase0Points"
-            / "lightning_logs"
-            / f"version_{version_num}"
-            / "checkpoints"
-            / f"{ckpt_name}.ckpt"
-        )
+        ckpt_path = get_best_ckpt_path(MODEL_CHECKPOINTS, args.version_num)
         model = CNNModulePhase0Points().load_from_checkpoint(ckpt_path)
 
     checkpoint_callback = ModelCheckpoint(
         monitor="val_loss",
-        filename="resnet-{epoch:02d}-{val_loss:.5f}",
+        filename="phase0-{epoch:02d}-{val_loss:.5f}",
         save_top_k=3,
         mode="min",
         save_last=True,
     )
 
     trainer = Trainer(
-        default_root_dir=Path(args.model_checkpoints) / "CNNModulePhase0Points",
+        default_root_dir=MODEL_CHECKPOINTS,
         accelerator="gpu",
         max_epochs=args.max_epochs,
         callbacks=[checkpoint_callback],
@@ -75,25 +66,19 @@ if __name__ == "__main__":
     parser.add_argument(
         "--train_data",
         type=str,
-        default="/home/avandavad/projects/receipt_extractor/data/train",
+        default=str(PROJ_DIR / "data" / "train"),
+        help="path to training data",
     )
     parser.add_argument(
         "--val_data",
         type=str,
-        default="/home/avandavad/projects/receipt_extractor/data/val",
+        default=str(PROJ_DIR / "data" / "val"),
+        help="path to validation data",
     )
-    parser.add_argument("--from_scratch", action="store_true")
-    parser.add_argument("--version_num", type=int, default=0)
-    parser.add_argument(
-        "--ckpt_name", type=str, default="resnet-epoch=65-val_loss=0.00149"
-    )
-    parser.add_argument(
-        "--model_checkpoints",
-        type=str,
-        default="/home/avandavad/projects/receipt_extractor/model_checkpoints",
-    )
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--max_epochs", type=int, default=1000)
+    parser.add_argument("--from_scratch", action="store_true", help="train from scratch")
+    parser.add_argument("--version_num", type=int, default=-1, help="continue training from this version")
+    parser.add_argument("--batch_size", type=int, default=32, help="batch size")
+    parser.add_argument("--max_epochs", type=int, default=1000, help="max epochs")
 
     args = parser.parse_args()
 
