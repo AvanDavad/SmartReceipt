58e932060c68e3cad3b9bc92474cee68030bf22b
Ivan, David <avandavad@gmail.com>
2024-01-03 12:12:28 +0100

    feat: dropout for phase0 backbone network

--- src/models/phase0_points/backbone.py
+++ src/models/phase0_points/backbone.py
@@ -102,6 +102,8 @@ class Phase0PointsBackbone(nn.Module):
             32, 32, kernel_size=3, stride=1, padding="valid", bias=True
         )
 
+        self.dropout = nn.Dropout(p=0.0)
+
     def forward(self, x):
         x = x0 = self.conv_1(x)  # 768x768x8
         x = self.relu(x)  # 768x768x8
@@ -109,7 +111,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 768x768x8
         x = self.conv_3(x)  # 768x768x8
         x = self.relu(x)  # 768x768x8
-        x = x0 + x  # 768x768x8
+        x = self.dropout(x + x0)  # 768x768x8
 
         x = x0 = self.avg_pool(x)  # 384x384x8
         x = self.conv_4(x)  # 384x384x8
@@ -118,7 +120,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 384x384x8
         x = self.conv_6(x)  # 384x384x8
         x = self.relu(x)  # 384x384x8
-        x = x0 + x  # 384x384x8
+        x = self.dropout(x + x0)  # 384x384x8
 
         x = self.avg_pool(x)  # 192x192x8
         x = x0 = self.conv_7(x)  # 192x192x16
@@ -127,7 +129,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 192x192x16
         x = self.conv_9(x)  # 192x192x16
         x = self.relu(x)  # 192x192x16
-        x = x0 + x  # 192x192x16
+        x = self.dropout(x + x0)  # 192x192x16
 
         x = x0 = self.avg_pool(x)  # 96x96x16
         x = self.conv_10(x)  # 96x96x16
@@ -136,7 +138,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 96x96x16
         x = self.conv_12(x)  # 96x96x16
         x = self.relu(x)  # 96x96x16
-        x = x0 + x  # 96x96x16
+        x = self.dropout(x + x0)  # 96x96x16
 
         x = x0 = self.avg_pool(x)  # 48x48x16
         x = self.conv_13(x)  # 48x48x16
@@ -145,7 +147,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 48x48x16
         x = self.conv_15(x)  # 48x48x16
         x = self.relu(x)  # 48x48x16
-        x = x0 + x  # 48x48x16
+        x = self.dropout(x + x0)  # 48x48x16
 
         x = self.avg_pool(x)  # 24x24x16
         x = x0 = self.conv_16(x)  # 24x24x32
@@ -154,7 +156,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 24x24x32
         x = self.conv_18(x)  # 24x24x32
         x = self.relu(x)  # 24x24x32
-        x = x0 + x  # 24x24x32
+        x = self.dropout(x + x0)  # 24x24x32
 
         x = x0 = self.avg_pool(x)  # 12x12x32
         x = self.conv_19(x)  # 12x12x32
@@ -163,7 +165,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 12x12x32
         x = self.conv_21(x)  # 12x12x32
         x = self.relu(x)  # 12x12x32
-        x = x0 + x  # 12x12x32
+        x = self.dropout(x + x0)  # 12x12x32
 
         x = x0 = self.avg_pool(x)  # 6x6x32
         x = self.conv_22(x)  # 6x6x32
@@ -172,7 +174,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 6x6x32
         x = self.conv_24(x)  # 6x6x32
         x = self.relu(x)  # 6x6x32
-        x = x0 + x  # 6x6x32
+        x = self.dropout(x + x0)  # 6x6x32
 
         x = x0 = self.avg_pool(x)  # 3x3x32
         x = self.conv_25(x)  # 3x3x32
@@ -181,7 +183,7 @@ class Phase0PointsBackbone(nn.Module):
         x = self.relu(x)  # 3x3x32
         x = self.conv_27(x)  # 3x3x32
         x = self.relu(x)  # 3x3x32
-        x = x0 + x  # 3x3x32
+        x = self.dropout(x + x0)  # 3x3x32
 
         x = self.final_conv(x)  # 1x1x32
 
